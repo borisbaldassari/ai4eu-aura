558a4594aad0fe4fb5ed2cc0ac00b2fac99b811c
Boris Baldassari <boris.baldassari@eclipse-foundation.org>
2021-11-11 22:19:34 +0100

    Update checks to target src directory.

---
+++ resources/releng/check_repository.sh
@@ -0,0 +1,7 @@
+
+echo "Using flake8 to check Python code"
+flake8 ./src/*/*.py
+echo "Using black to check Python code"
+black --check ./src/*/*.py
+echo "Using flake8 to check tests on src/"
+pytest --cov=./src/ ./src/
