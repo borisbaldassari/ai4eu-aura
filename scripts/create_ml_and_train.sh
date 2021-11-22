6fc648122fa7ce4dfa5b0dbd74bc813a316c3f72
Boris Baldassari <boris.baldassari@eclipse-foundation.org>
2021-11-22 09:59:10 +0100

    Reformat scripts for readability

--- scripts/create_ml_and_train.sh
+++ scripts/create_ml_and_train.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-. env/bin/activate
-python3 src/usecase/create_ml_dataset.py --input-folder output/cons-v0_6 --output-folder output/ml_dataset
-python3 src/usecase/train_model.py --ml-dataset-path output/ml_dataset/df_ml.csv
+DATA_PATH=$1
+
+#. env/bin/activate
+
+echo "- Create ML Dataset"
+python3 src/usecase/create_ml_dataset.py \
+	--input-folder ${DATA_PATH}/cons-v0_6 \
+	--output-folder ${DATA_PATH}/ml_dataset
+
+echo "- Train model."
+python3 src/usecase/train_model.py \
+	--ml-dataset-path ${DATA_PATH}/ml_dataset/df_ml.csv
--- scripts/run_bash_pipeline.sh
+++ scripts/run_bash_pipeline.sh
@@ -9,16 +9,23 @@ mkdir -p ${EXPORT_BASE}/res-v0_6/${EXPORT_PATH}/
 mkdir -p ${EXPORT_BASE}/feats-v0_6/${EXPORT_PATH}/
 mkdir -p ${EXPORT_BASE}/cons-v0_6/${EXPORT_PATH}/
 
-echo "# In $DATA_PATH - Out $EXPORT_PATH"
+echo "# Run dataprep pipeline - In $DATA_PATH - Out $EXPORT_PATH"
 
 echo "- Detect rr-intervals."
-./scripts/bash_pipeline/1_detect_qrs_wrapper.sh  -i ${DATA_PATH} -o ${EXPORT_BASE}/res-v0_6/${EXPORT_PATH}
+./scripts/bash_pipeline/1_detect_qrs_wrapper.sh  \
+	-i ${DATA_PATH} \
+	-o ${EXPORT_BASE}/res-v0_6/${EXPORT_PATH}
 
 echo "- Compute features."
-./scripts/bash_pipeline/2_compute_hrvanalysis_features_wrapper.sh  -i ${EXPORT_BASE}/res-v0_6/${EXPORT_PATH} -o ${EXPORT_BASE}/feats-v0_6/${EXPORT_PATH}
+./scripts/bash_pipeline/2_compute_hrvanalysis_features_wrapper.sh \
+	-i ${EXPORT_BASE}/res-v0_6/${EXPORT_PATH} \
+	-o ${EXPORT_BASE}/feats-v0_6/${EXPORT_PATH}
 
 echo "- Consolidate features and annotations."
-./scripts/bash_pipeline/3_consolidate_feats_and_annot_wrapper.sh  -i ${EXPORT_BASE}/feats-v0_6/${EXPORT_PATH} -a ${DATA_PATH} -o ${EXPORT_BASE}/cons-v0_6/${EXPORT_PATH}
+./scripts/bash_pipeline/3_consolidate_feats_and_annot_wrapper.sh  \
+	-i ${EXPORT_BASE}/feats-v0_6/${EXPORT_PATH} \
+	-a ${DATA_PATH} \
+	-o ${EXPORT_BASE}/cons-v0_6/${EXPORT_PATH}
 
 echo "Done"
 
